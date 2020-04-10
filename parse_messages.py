23ce9470aa212931dc0899642b50ff3623292fc4
UnrealLink <37017725+UnrealLink@users.noreply.github.com>
2020-04-10 15:02:32 +0200

    some slight changes

--- parse_messages.py
+++ parse_messages.py
@@ -22,11 +22,12 @@ def parse_messages(file):
 
     with open(file, 'r', encoding='utf8') as f:
         data = json.load(f)
+    participants = [elm['name'] for elm in data['participants'] if elm['name'] != ME]
     for message in data['messages']:
         parsed_message = parse_message(message)
         if parsed_message:
             messages.append(parsed_message)
-    participants = [elm['name'] for elm in data['participants'] if elm['name'] != ME]
+    
 
     return {'participants': participants, 'messages': messages}
 
@@ -34,9 +35,10 @@ def parse_all_messages(name=ME, dir=DATA_PATH, message_file=MESSAGE_FILE, no_gro
     all_messages = {}
     t = time.time()
     for d in os.listdir(dir):
-        res = parse_messages(f"{dir}\\{d}\\{message_file}")
-        if len(res['participants']) == 1:
-            all_messages[res['participants'][0]] = res['messages']
+        for f in [f for f in os.listdir(f"{dir}\\{d}") if f.startswith("message")]:
+            res = parse_messages(f"{dir}\\{d}\\{f}")
+            if len(res['participants']) <= 1:
+                all_messages[",".join(res['participants'])] = all_messages.get(",".join(res['participants']), []) + res['messages']
     print(f"Parsed all messages in {time.time()-t}s.")
     return all_messages
 
@@ -64,15 +66,23 @@ def show_evolution(convos, max_date=datetime(2019, 10, 9),
         date -= bin
     coords = matplotlib.dates.date2num(dates)
     for (name, messages) in convos:
+        messages = sorted(messages, key=lambda x : x.date, reverse=True)
         values = get_values(messages, dates)
         plt.plot_date(coords, values, '-', label=name)
     plt.legend()
     plt.show()
 
-def parse_and_show_messages(name, data_path, min_date, max_date, bin, min_rank, max_rank):
+def parse_and_show_messages(name=ME, data_path=DATA_PATH, max_date=datetime(2019, 9, 15), 
+                            min_date=datetime(2013, 9, 1), bin=timedelta(days=7), min_rank=6, max_rank=7):
     messages = parse_all_messages(name, data_path)
     sorted_messages = sorted(messages.items(), key=lambda x : len(x[1]), reverse=True)
-    show_evolution(sorted_messages[int(min_rank):int(max_rank)], 
-                   datetime(*list(map(int, max_date.split('/')))),
-                   datetime(*list(map(int, min_date.split('/')))),
-                   timedelta(days=int(bin)))
+    # show_evolution(sorted_messages[int(min_rank):int(max_rank)], 
+    #                datetime(*list(map(int, max_date.split('/')))),
+    #                datetime(*list(map(int, min_date.split('/')))),
+    #                timedelta(days=int(bin)))
+    for (name, messages) in sorted_messages[min_rank:max_rank]:
+        print(f"{name} : {len(messages)}")
+    show_evolution(sorted_messages[min_rank:max_rank], max_date, min_date, bin)
+
+if __name__ == "__main__":
+    parse_and_show_messages()
